9c2aa8a89c3136b33619770c0fed8fea933b1a90
kiritoEM <johankirito64@gmail.com>
2023-11-14 08:14:32 +0100

    first commit

--- components/landing/LandingRecent.tsx
+++ components/landing/LandingRecent.tsx
@@ -1,16 +1,18 @@
 import arrayLandingHelpers from "@/helpers/arrayLandingHelpers";
+import LandingRecentContent from "./components/LandingRecentContent";
 
 const LandingRecent = (): JSX.Element => {
-    const {landingRecentData} = arrayLandingHelpers();
-    return (
-      <section className="landing-recent">
-        <div className="landing-recent__container">
-          <div className="section-title">
-            <h3>{landingRecentData.title}</h3>
-          </div>
+  const { landingRecentData } = arrayLandingHelpers();
+  return (
+    <section className="landing-recent">
+      <div className="landing-recent__container">
+        <div className="section-header">
+          <h3>{landingRecentData.title}</h3>
         </div>
-      </section>
-    );
+        <LandingRecentContent />
+      </div>
+    </section>
+  );
 };
 
 export default LandingRecent;
---
+++ components/landing/components/LandingRecentContent.tsx
@@ -0,0 +1,16 @@
+
+const LandingRecentContent = () : JSX.Element => {
+    return (
+        <div className="section-content mt-4">
+            <div className="row">
+                 <div className="col-4">
+                     <div className="box">
+                         <div className="box__image"></div>
+                     </div>
+                 </div>
+            </div>
+        </div>
+    );
+};
+
+export default LandingRecentContent;
