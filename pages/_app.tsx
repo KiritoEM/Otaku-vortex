97120a46a3bcd355c61a1cc964343d7907d3fcfe
kiritoEM <johankirito64@gmail.com>
2023-11-16 19:44:00 +0100

    add loader to page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 500);
+    }, 1550);
   }, [loading]);
 
   return (
