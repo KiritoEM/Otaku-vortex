bea6c709149e871ae39f2bc106179517fc6a2148
kiritoEM <johankirito64@gmail.com>
2023-11-20 09:04:51 +0100

    add signupController node js

--- Server/controller/AuthController.js
+++ Server/controller/AuthController.js
@@ -1,17 +1,30 @@
-const { signupHelper } = require("../helper/AuthHelper");
+const {
+  signupHelper,
+  hashPassword,
+  checkEmail,
+} = require("../helper/AuthHelper");
 
 const signupController = async (req, res) => {
   const { username, email, password } = req.body;
 
-  console.log(req.body);
+  //hash password
+  const hashedPassword = await hashPassword(password);
+  const emailExists = await checkEmail(email)
 
   try {
-    const response = await signupHelper(username, email, password);
+    if (!emailExists) {
+      const response = await signupHelper(username, email, hashedPassword);
 
-    if (response) {
-      res.status(200).json({ message: "Inscription réussie" });
-    } else {
-      res.status(500).json({ message: "Echec de l' inscription" });
+      if (response) {
+        res.status(200).json({ message: "Inscription réussie", response });
+      } else {
+        res.status(500).json({ message: "Echec de l' inscription" });
+      }
+    }
+     else {
+      res.json({
+        message: "l' utilisateur s'est déja inscris avec cet email",
+      });
     }
   } catch (err) {
     console.log("erreur: ", err);
--- Server/helper/AuthHelper.js
+++ Server/helper/AuthHelper.js
@@ -1,4 +1,5 @@
-const userModel = require("./../models/userModels");
+const userModel = require("../models/userModels");
+const bcrypt = require("bcrypt");
 
 const signupHelper = (username, email, password) => {
   const newUser = new userModel({
@@ -10,4 +11,30 @@ const signupHelper = (username, email, password) => {
   return newUser.save();
 };
 
-module.exports = { signupHelper };
+const hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    return hashedPassword;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const checkEmail = async (email) => {
+  try {
+    const user = await userModel.findOne({ email: email });
+
+    if (user) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+
+module.exports = { signupHelper, hashPassword, checkEmail };
