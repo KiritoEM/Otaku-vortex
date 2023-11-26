fc5629e4dd0b314432a3bc43ef0a170dc99d41aa
kiritoEM <johankirito64@gmail.com>
2023-11-26 05:48:32 +0100

    addsocket.io to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 1420);
+    }, 1820);
   }, [loading]);
 
   return (
